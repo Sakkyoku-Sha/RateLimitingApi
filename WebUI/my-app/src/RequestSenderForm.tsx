adaadb1271e56952db5ba8e277aadea72da65eb9
RyanAnderson <RyanAnderson0147@gmail.com>
2022-11-15 09:15:35 -0700

    Make UI Actually send N Get Requests.

--- WebUI/my-app/src/RequestSenderForm.tsx
+++ WebUI/my-app/src/RequestSenderForm.tsx
@@ -1,24 +1,27 @@
 import {useCallback, useState} from "react";
-import {Box, Button, TextField} from "@mui/material";
+import {Box, Button, TextField, Typography} from "@mui/material";
 
 const Form = () => {
     
     const [url, setUrl] = useState<string>("http://localhost:5072/api/Rate"); 
     const [numberOfRequests, setNumberOfRequests] = useState<number>(1)
     
-    const sendRequest = useCallback(() => {
-        
-        return fetch(url).then((response) => response.json())
-            .then((data) => console.log(data));
-    }, [url])
+    const sendRequests = useCallback(() => {
+        for(let i = 0; i < numberOfRequests; i++){
+            fetch(url).then((response) => response.json())
+                .then((data) => console.log(data));
+        }
+    }, [url, numberOfRequests])
     
-    return <Box alignSelf={'center'} component="form" sx={{ 
+    return <>
+        <Box alignSelf={'center'} component="form" sx={{ 
         '& .MuiTextField-root': { m: 1, width: '25ch' },
     }}>
-        <TextField required={true} type="url" id="outlined-basic" label={"url"} onChange={value => setUrl(value.target.value)} value={url}/>
-        <TextField required={true} type="number" id="" label="# of requests" value={numberOfRequests} onChange={value => setNumberOfRequests(Number.parseInt(value.target.value))} />
-        <Button variant="contained" form={"form"} type={"submit"} onClick={sendRequest}>Send</Button>
-    </Box>
+            <TextField required={true} type="url" id="outlined-basic" label={"url"} onChange={value => setUrl(value.target.value)} value={url}/>
+            <TextField required={true} type="number" id="" label="# of requests" value={numberOfRequests} onChange={value => setNumberOfRequests(Number.parseInt(value.target.value))} />
+            <Button variant="contained" form={"form"} type={"submit"} onClick={sendRequests}>Send</Button>
+        </Box>
+    </>
 }
 
 
